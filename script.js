31ffce52465c4db6013c9fecab096899ca309a79
Frederico Gonçalves <64445839+fredsilva7l@users.noreply.github.com>
2023-12-25 05:43:35 -0300

    adicionado arquivos online
    adicionado arquivos online

---
+++ script.js
@@ -0,0 +1,592 @@
+const dados = [
+  {
+    data: "2024-04-04",
+    valor: -1,
+    texto: "",
+  },
+  {
+    data: "2024-04-03",
+    valor: 0,
+    texto: "",
+  },
+  {
+    data: "2024-04-02",
+    valor: 1,
+    texto: "",
+  },
+  {
+    data: "2024-04-01",
+    valor: 2,
+    texto: "",
+  },
+  {
+    data: "2024-03-31",
+    valor: 3,
+    texto: "",
+  },
+  {
+    data: "2024-03-30",
+    valor: 4,
+    texto: "",
+  },
+  {
+    data: "2024-03-29",
+    valor: 5,
+    texto: "",
+  },
+  {
+    data: "2024-03-28",
+    valor: 6,
+    texto: "",
+  },
+  {
+    data: "2024-03-27",
+    valor: 7,
+    texto: "",
+  },
+  {
+    data: "2024-03-26",
+    valor: 8,
+    texto: "",
+  },
+  {
+    data: "2024-03-25",
+    valor: 9,
+    texto: "",
+  },
+  {
+    data: "2024-03-24",
+    valor: 10,
+    texto: "",
+  },
+  {
+    data: "2024-03-23",
+    valor: 11,
+    texto: "",
+  },
+  {
+    data: "2024-03-22",
+    valor: 12,
+    texto: "",
+  },
+  {
+    data: "2024-03-21",
+    valor: 13,
+    texto: "",
+  },
+  {
+    data: "2024-03-20",
+    valor: 14,
+    texto: "",
+  },
+  {
+    data: "2024-03-19",
+    valor: 15,
+    texto: "",
+  },
+  {
+    data: "2024-03-18",
+    valor: 16,
+    texto: "",
+  },
+  {
+    data: "2024-03-17",
+    valor: 17,
+    texto: "",
+  },
+  {
+    data: "2024-03-16",
+    valor: 18,
+    texto: "",
+  },
+  {
+    data: "2024-03-15",
+    valor: 19,
+    texto: "",
+  },
+  {
+    data: "2024-03-14",
+    valor: 20,
+    texto: "",
+  },
+  {
+    data: "2024-03-13",
+    valor: 21,
+    texto: "",
+  },
+  {
+    data: "2024-03-12",
+    valor: 22,
+    texto: "",
+  },
+  {
+    data: "2024-03-11",
+    valor: 23,
+    texto: "",
+  },
+  {
+    data: "2024-03-10",
+    valor: 24,
+    texto: "",
+  },
+  {
+    data: "2024-03-09",
+    valor: 25,
+    texto: "",
+  },
+  {
+    data: "2024-03-08",
+    valor: 26,
+    texto: "",
+  },
+  {
+    data: "2024-03-07",
+    valor: 27,
+    texto: "",
+  },
+  {
+    data: "2024-03-06",
+    valor: 28,
+    texto: "",
+  },
+  {
+    data: "2024-03-05",
+    valor: 29,
+    texto: "",
+  },
+  {
+    data: "2024-03-04",
+    valor: 30,
+    texto: "",
+  },
+  {
+    data: "2024-03-03",
+    valor: 31,
+    texto: "",
+  },
+  {
+    data: "2024-03-02",
+    valor: 32,
+    texto: "",
+  },
+  {
+    data: "2024-03-01",
+    valor: 33,
+    texto: "",
+  },
+  {
+    data: "2024-02-29",
+    valor: 34,
+    texto: "",
+  },
+  {
+    data: "2024-02-28",
+    valor: 35,
+    texto: "",
+  },
+  {
+    data: "2024-02-27",
+    valor: 36,
+    texto: "",
+  },
+  {
+    data: "2024-02-26",
+    valor: 37,
+    texto: "",
+  },
+  {
+    data: "2024-02-25",
+    valor: 38,
+    texto: "",
+  },
+  {
+    data: "2024-02-24",
+    valor: 39,
+    texto: "",
+  },
+  {
+    data: "2024-02-23",
+    valor: 40,
+    texto: "",
+  },
+  {
+    data: "2024-02-22",
+    valor: 41,
+    texto: "",
+  },
+  {
+    data: "2024-02-21",
+    valor: 42,
+    texto: "",
+  },
+  {
+    data: "2024-02-20",
+    valor: 43,
+    texto: "",
+  },
+  {
+    data: "2024-02-19",
+    valor: 44,
+    texto: "",
+  },
+  {
+    data: "2024-02-18",
+    valor: 45,
+    texto: "",
+  },
+  {
+    data: "2024-02-17",
+    valor: 46,
+    texto: "",
+  },
+  {
+    data: "2024-02-16",
+    valor: 47,
+    texto: "",
+  },
+  {
+    data: "2024-02-15",
+    valor: 48,
+    texto: "",
+  },
+  {
+    data: "2024-02-14",
+    valor: 49,
+    texto: "",
+  },
+  {
+    data: "2024-02-13",
+    valor: 50,
+    texto: "",
+  },
+  {
+    data: "2024-02-12",
+    valor: 51,
+    texto: "",
+  },
+  {
+    data: "2024-02-11",
+    valor: 52,
+    texto: "",
+  },
+  {
+    data: "2024-02-10",
+    valor: 53,
+    texto: "",
+  },
+  {
+    data: "2024-02-09",
+    valor: 54,
+    texto: "",
+  },
+  {
+    data: "2024-02-08",
+    valor: 55,
+    texto: "",
+  },
+  {
+    data: "2024-02-07",
+    valor: 56,
+    texto: "",
+  },
+  {
+    data: "2024-02-06",
+    valor: 57,
+    texto: "",
+  },
+  {
+    data: "2024-02-05",
+    valor: 58,
+    texto: "",
+  },
+  {
+    data: "2024-02-04",
+    valor: 59,
+    texto: "",
+  },
+  {
+    data: "2024-02-03",
+    valor: 60,
+    texto: "",
+  },
+  {
+    data: "2024-02-02",
+    valor: 61,
+    texto: "",
+  },
+  {
+    data: "2024-02-01",
+    valor: 62,
+    texto: "",
+  },
+  {
+    data: "2024-01-31",
+    valor: 63,
+    texto: "",
+  },
+  {
+    data: "2024-01-30",
+    valor: 64,
+    texto: "",
+  },
+  {
+    data: "2024-01-29",
+    valor: 65,
+    texto: "",
+  },
+  {
+    data: "2024-01-28",
+    valor: 66,
+    texto: "",
+  },
+  {
+    data: "2024-01-27",
+    valor: 67,
+    texto: "",
+  },
+  {
+    data: "2024-01-26",
+    valor: 68,
+    texto: "",
+  },
+  {
+    data: "2024-01-25",
+    valor: 69,
+    texto: "",
+  },
+  {
+    data: "2024-01-24",
+    valor: 70,
+    texto: "",
+  },
+  {
+    data: "2024-01-23",
+    valor: 71,
+    texto: "",
+  },
+  {
+    data: "2024-01-22",
+    valor: 72,
+    texto: "",
+  },
+  {
+    data: "2024-01-21",
+    valor: 73,
+    texto: "",
+  },
+  {
+    data: "2024-01-20",
+    valor: 74,
+    texto: "",
+  },
+  {
+    data: "2024-01-19",
+    valor: 75,
+    texto: "",
+  },
+  {
+    data: "2024-01-18",
+    valor: 76,
+    texto: "",
+  },
+  {
+    data: "2024-01-17",
+    valor: 77,
+    texto: "",
+  },
+  {
+    data: "2024-01-16",
+    valor: 78,
+    texto: "",
+  },
+  {
+    data: "2024-01-15",
+    valor: 79,
+    texto: "",
+  },
+  {
+    data: "2024-01-14",
+    valor: 80,
+    texto: "",
+  },
+  {
+    data: "2024-01-13",
+    valor: 81,
+    texto: "",
+  },
+  {
+    data: "2024-01-12",
+    valor: 82,
+    texto: "",
+  },
+  {
+    data: "2024-01-11",
+    valor: 83,
+    texto: "",
+  },
+  {
+    data: "2024-01-10",
+    valor: 84,
+    texto: "",
+  },
+  {
+    data: "2024-01-09",
+    valor: 85,
+    texto: "",
+  },
+  {
+    data: "2024-01-08",
+    valor: 86,
+    texto: "",
+  },
+  {
+    data: "2024-01-07",
+    valor: 87,
+    texto: "",
+  },
+  {
+    data: "2024-01-06",
+    valor: 88,
+    texto: "",
+  },
+  {
+    data: "2024-01-05",
+    valor: 89,
+    texto: "",
+  },
+  {
+    data: "2024-01-04",
+    valor: 90,
+    texto: "",
+  },
+  {
+    data: "2024-01-03",
+    valor: 91,
+    texto: "",
+  },
+  {
+    data: "2024-01-02",
+    valor: 92,
+    texto: "",
+  },
+  {
+    data: "2024-01-01",
+    valor: 93,
+    texto: "",
+  },
+  {
+    data: "2023-12-31",
+    valor: 94,
+    texto: "",
+  },
+  {
+    data: "2023-12-30",
+    valor: 95,
+    texto: "",
+  },
+  {
+    data: "2023-12-29",
+    valor: 96,
+    texto: "",
+  },
+  {
+    data: "2023-12-28",
+    valor: 97,
+    texto: "estou gostando",
+  },
+  {
+    data: "2023-12-27",
+    valor: 98,
+    texto: "oi deu certo",
+  },
+  {
+    data: "2023-12-26",
+    valor: 99,
+    texto: "teste",
+  },
+  {
+    data: "2023-12-25",
+    valor: 100,
+    texto: "entrei aqui minha linda",
+    link: "audio/lua.mp3",
+  },
+];
+
+const s = 1000,
+  m = s * 60,
+  h = m * 60,
+  d = h * 24,
+  y = d * 366;
+
+function counting() {
+  const days = document.getElementById("days"),
+    hours = document.getElementById("hours"),
+    minutes = document.getElementById("minutes"),
+    seconds = document.getElementById("seconds"),
+    timeNow = new Date().getTime(),
+    setDate = new Date("04/04/2024").getTime(),
+    diff = setDate - timeNow;
+
+  days.innerText = Math.floor(diff / d);
+  hours.innerText = Math.floor((diff % d) / h);
+  minutes.innerText = Math.floor((diff % h) / m);
+  seconds.innerText = Math.floor((diff % m) / s);
+
+  return Math.floor(diff / d);
+}
+
+counting();
+comparaDatas();
+setInterval(() => {
+  counting();
+}, 1000);
+
+function comparaDatas() {
+  let armazenaDia = counting();
+  let texto = document.getElementById("conteudo");
+
+  dados.forEach((compara) => {
+    if (compara.valor == armazenaDia - 0) {
+      texto.textContent = compara.texto;
+      return compara.texto;
+    }
+  });
+}
+
+const formAddDate = document.getElementById("addDate");
+formAddDate.addEventListener("submit", addTime);
+
+function addTime(e) {
+  const date = document.getElementById("date").value;
+  let armazenaDia = counting();
+  let texto = document.getElementById("until");
+
+  const audio = document.querySelector("audio");
+  audio.play();
+
+  dados.forEach((compara) => {
+    if (armazenaDia <= compara.valor) {
+      if (date == compara.data) {
+        texto.textContent = compara.texto;
+
+        console.log(compara.link);
+        console.log(compara.texto);
+
+        const sources = document.querySelectorAll("audio source");
+        for (const source of sources) {
+          const src = source.src;
+          console.log(src); // "audio/lua.mp3"
+
+          const path = src.slice(0, src.length);
+          const filename = "nova-musica.mp3";
+
+          const newSrc = `${path}/${filename}`;
+          console.log(newSrc)
+        }
+
+        return null;
+      }
+    } else texto.textContent = "Data aceitas de 25/12/2023 até 04/04/2024";
+  });
+  e.preventDefault();
+}
